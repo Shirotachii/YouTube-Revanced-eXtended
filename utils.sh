77ac77d64739b5c5f4f93606aafe4d6ac16403dd
Shiro <ryukenzych@gmail.com>
2025-08-02 17:32:59 +0700

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -637,7 +637,7 @@ module_prop() {
 name=${2}
 version=v${3}
 versionCode=${NEXT_VER_CODE}
-author=j-hc
+author=Shirotachi
 description=${4}" >"${6}/module.prop"
 
 	if [ "$ENABLE_MAGISK_UPDATE" = true ]; then echo "updateJson=${5}" >>"${6}/module.prop"; fi
